564a08c9b3342869943d9f8e81d8fc0f8dafb8bb
Darren Victoriano <darren.victoriano@gmail.com>
2018-12-16 13:32:59 -0800

    working attack btn

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,9 @@
 // Global Variables
 /////////////////////////////////////////////
 var characters = [],
-    gameData = {};
+    gameData = {},
+    progressIntHero = null,
+    progressIntEnemy = null;
 
 
 /////////////////////////////////////////////
@@ -17,12 +19,10 @@ function varInit() {
                 current: 500,
                 total: 500
             },
-            attack: [
-                {
-                    name: "Pika Lightning",
-                    damage: randomNum(20, 50)
-                }
-            ],
+            attack: {
+                name: "Pika Lightning",
+                damage: randomNum(20, 50)
+            },
             image: {
                 icon: "./assets/images/pika.png",
                 front: "./assets/images/pika-front.gif",
@@ -36,12 +36,10 @@ function varInit() {
                 current: 500,
                 total: 500
             },
-            attack: [
-                {
-                    name: "Charmander Fire",
-                    damage: randomNum(20, 50)
-                }
-            ],
+            attack: {
+                name: "Charmander Fire",
+                damage: randomNum(20, 50)
+            },
             image: {
                 icon: "./assets/images/char.png",
                 front: "./assets/images/char-front.gif",
@@ -55,12 +53,10 @@ function varInit() {
                 current: 500,
                 total: 500
             },
-            attack: [
-                {
-                    name: "Squirtle Squirt",
-                    damage: randomNum(20, 50)
-                }
-            ],
+            attack: {
+                name: "Squirtle Squirt",
+                damage: randomNum(20, 50)
+            },
             image: {
                 icon: "./assets/images/tur.png",
                 front: "./assets/images/tur-front.gif",
@@ -74,12 +70,10 @@ function varInit() {
                 current: 500,
                 total: 500
             },
-            attack: [
-                {
-                    name: "Bulba Bur",
-                    damage: randomNum(20, 50)
-                }
-            ],
+            attack: {
+                name: "Bulba Bur",
+                damage: randomNum(20, 50)
+            },
             image: {
                 icon: "./assets/images/bulba.png",
                 front: "./assets/images/bulba-front.gif",
@@ -119,6 +113,82 @@ function heroAlignment(name, alignment) {
 
 }
 
+function attackTheEnemy() {
+    gameData.enemy.hp.current -= gameData.hero.attack.damage;
+
+    if (gameData.enemy.hp.current <= 0) {
+        // enemy is dead
+        gameData.step = 2;
+
+        //remove dead
+        $("#enemy-hp-div").empty();
+        $("#enemy-hp-div").removeClass("hp-stat");
+        $("#enemy").empty();
+
+        //show character select
+        $("#char-select").addClass("char-select");
+        $(".char-container").show();
+    } else {
+        // enemy is still alive, kill!
+
+        //animate health bar
+        progressIntEnemy = setInterval(function () {
+
+            // get the value of enemys health bar
+            var val = $("#enemy-progress").val();
+            val--;
+
+            // update the health bar value
+            $("#enemy-progress").val(val);
+            $("#enemy-hp").text(gameData.enemy.hp.current + "/" + gameData.enemy.hp.total);
+
+            // stop animation after the attack
+            if (val == gameData.enemy.hp.current) {
+                clearInterval(progressIntEnemy);
+
+            }
+        }, 1);
+    }
+}
+
+function attackTheHero() {
+    gameData.hero.hp.current -= gameData.enemy.attack.damage;
+
+    if (gameData.hero.hp.current <= 0) {
+        // hero is dead
+        gameData.step = 2;
+
+        //remove dead
+        $("#hero-hp-div").empty();
+        $("#hero-hp-div").removeClass("hp-stat");
+        $("#hero").empty();
+
+        //show character select
+        $("#char-select").addClass("char-select");
+        $(".char-container").show();
+    } else {
+        // hero is still alive
+
+        //animate health bar
+        progressIntHero = setInterval(function () {
+
+            // get the value of enemys health bar
+            var val = $("#hero-progress").val();
+            val--;
+
+            // update the health bar value
+            $("#hero-progress").val(val);
+            $("#hero-hp").text(gameData.hero.hp.current + "/" + gameData.hero.hp.total);
+
+            // stop animation after the attack
+            if (val == gameData.hero.hp.current) {
+                clearInterval(progressIntHero);
+
+            }
+        }, 1);
+    }
+}
+
 
 /////////////////////////////////////////////
 // Game Interface
@@ -149,72 +219,83 @@ function createHPbar(alignment) {
         $("#hero-hp-div").addClass("hp-stat");
         $("#hero-hp-div").append("<p id='hero-name' class='m-0'>" + gameData.hero.name + "</p><progress id='hero-progress' max='" + gameData.hero.hp.current + "'></progress><div id='hero-hp'></div>");
         $("#hero-hp").text(gameData.hero.hp.current + "/" + gameData.hero.hp.total);
+        $("#hero-progress").val(gameData.hero.hp.current);
     } else if (alignment == "enemy") {
         $("#enemy-hp-div").addClass("hp-stat");
         $("#enemy-hp-div").append("<p id='enemy-name' class='m-0'>" + gameData.enemy.name + "</p><progress id='enemy-progress' max='" + gameData.enemy.hp.current + "'></progress><div id='enemy-hp'></div>");
         $("#enemy-hp").text(gameData.enemy.hp.current + "/" + gameData.enemy.hp.total);
-
+        $("#enemy-progress").val(gameData.enemy.hp.current);
     }
 }
 
 function showAttackBTN() {
-    $("#attack-div").append("<button id='attack-btn' class='btn btn-danger'>Attack that shit!</button>")
+    $("#attack-div").html("<button id='attack-btn' class='btn btn-danger'>Attack that shit!</button>")
 }
 
 
 /////////////////////////////////////////////
 // Start Game
 /////////////////////////////////////////////
+
+function startGame() {
+    $(".char-container").on("click", function () {
+
+        // get the image alt name which is a child of .char-container
+        var name = $(this).children("img").attr("alt");
+
+        switch (gameData.step) {
+
+            case 1:
+                // give the chosen heros to the gameData var
+                heroAlignment(name, "hero");
+
+                // remove the image after click
+                $(this).remove();
+
+                //create arena then
+                $("#arena").addClass("arena-set");
+                // move the image to the arena
+                populateArena($("#hero"), "hero");
+                // add hero hp bar to the arena
+                createHPbar("hero");
+
+                gameData.step = 2;
+                break;
+
+            case 2:
+
+                // give the character to the gameData var
+                heroAlignment(name, "enemy");
+
+                // remove the image after click
+                $(this).remove();
+
+                //hide remaining characters
+                $("#char-select").removeClass("char-select");
+                $(".char-container").hide();
+
+                // move the image to the arena
+                populateArena($("#enemy"), "enemy");
+                // add enemy hp bar to the arena
+                createHPbar("enemy");
+                //show attack button
+                showAttackBTN();
+
+                // update game step
+                gameData.step = 3;
+                break;
+        }
+
+    }); // end of characters clicks
+};
+
+
+
+$("#attack-div").on("click", "#attack-btn", function () {
+    attackTheEnemy();
+    attackTheHero();
+});
+
 varInit();
 populateCharSelect();
-
-$(".char-container").on("click", function () {
-
-    // get the image alt name which is a child of .char-container
-    var name = $(this).children("img").attr("alt");
-
-    switch (gameData.step) {
-
-        case 1:
-            // give the character to the gameData var
-            heroAlignment(name, "hero");
-
-            // remove the image after click
-            $(this).remove();
-
-            //create arena then
-            $("#arena").addClass("arena-set");
-            // move the image to the arena
-            populateArena($("#hero"), "hero");
-            // add hero hp bar to the arena
-            createHPbar("hero");
-
-            gameData.step = 2;
-            break;
-
-        case 2:
-            // give the character to the gameData var
-            heroAlignment(name, "enemy");
-
-            // remove the image after click
-            $(this).remove();
-
-            //hide remaining characters
-            $("#char-select").removeClass("char-select");
-            $(".char-container").hide();
-
-            // move the image to the arena
-            populateArena($("#enemy"), "enemy");
-            // add enemy hp bar to the arena
-            createHPbar("enemy");
-            //show attack button
-            showAttackBTN();
-    }
-
-
-    console.log(gameData.hero.name)
-    console.log(gameData.hero.attack[0].name)
-
-    console.log(gameData.enemy.name)
-    console.log(gameData.enemy.attack[0].name)
-});
+startGame();
